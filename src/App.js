ba618fb62d7f18843f4c9783f6299414d0aaf401
박남주 <qkrskawn2@gmail.com>
2023-09-05 15:41:36 +0900

    페스티벌 및 반복문예제

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import GlobalStyle from "./components/GlobalStyle";
+import Main from "./pages/Main";
+import Detail from "./pages/Detail";
+import Aside from "./components/Aside";
+import { ThemeProvider } from "styled-components";
+import { useState } from "react";
+// import Example from "./pages/Example";
+// import Example2 from "./pages/Example2";
+// import Example3 from "./pages/Example3";
+
+
 
 function App() {
+
+  const light = {
+    colors: {
+      Primary : "orange",
+      Secondary : "orangered"
+    }
+  }
+  const dark = {
+    colors : {
+      Primary : "#272929",
+      Secondary : "#e9e9e9"
+    }
+  }
+  
+  const [themeConfig, setThemeConfig] = useState("light");
+  const DarkMode = themeConfig === 'light' ? light : dark;
+  const ThemeSelect = ()=>{
+    setThemeConfig(themeConfig === 'light' ? 'dark' : 'light')
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={DarkMode}>
+        <GlobalStyle/>
+        <Aside ThemeSelect={ThemeSelect} themeConfig={themeConfig}/>
+        <Routes>
+          <Route path="/" element={<Main/>}></Route>
+          <Route path="/detail" element={<Detail/>}></Route>
+        </Routes>
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+@font-face {
+     font-family: 'S-CoreDream-3Light';
+     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff') format('woff');
+     font-weight: normal;
+     font-style: normal;
+}
+  *{margin: 0; padding: 0; font-family: 'S-CoreDream-3Light';}
+  ul{list-style:none}
+  a{text-decoration: none; color: #000;}
+`
+
+export default GlobalStyle
